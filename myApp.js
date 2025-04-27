2e8f671461e9ac15d158e4c1279756e19db22011
Gus <56235860+guslearn-GH@users.noreply.github.com>
2025-04-27 16:09:04 +0000

    #

--- myApp.js
+++ myApp.js
@@ -1,60 +1,73 @@
 require("dotenv").config();
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const mongoClient = require("mongodb");
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+const client = new mongoClient.MongoClient(process.env.MONGO_URI);
+async function run() {
+  try {
+    await client.connect();
+    console.log("Successful connection!!!");
 
-mongoose.connect(sample.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+    let Person;
 
-let Person;
+    const createAndSavePerson = (done) => {
+      done(null /*, data*/);
+    };
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
-};
+    const createManyPeople = (arrayOfPeople, done) => {
+      done(null /*, data*/);
+    };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
-};
+    const findPeopleByName = (personName, done) => {
+      done(null /*, data*/);
+    };
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
-};
+    const findOneByFood = (food, done) => {
+      done(null /*, data*/);
+    };
 
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
-};
+    const findPersonById = (personId, done) => {
+      done(null /*, data*/);
+    };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+    const findEditThenSave = (personId, done) => {
+      const foodToAdd = "hamburger";
 
-const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+      done(null /*, data*/);
+    };
 
-  done(null /*, data*/);
-};
+    const findAndUpdate = (personName, done) => {
+      const ageToSet = 20;
 
-const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
+      done(null /*, data*/);
+    };
 
-  done(null /*, data*/);
-};
+    const removeById = (personId, done) => {
+      done(null /*, data*/);
+    };
 
-const removeById = (personId, done) => {
-  done(null /*, data*/);
-};
+    const removeManyPeople = (done) => {
+      const nameToRemove = "Mary";
 
-const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
+      done(null /*, data*/);
+    };
 
-  done(null /*, data*/);
-};
+    const queryChain = (done) => {
+      const foodToSearch = "burrito";
 
-const queryChain = (done) => {
-  const foodToSearch = "burrito";
+      done(null /*, data*/);
+    };
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close(console.dir);
+  }
+}
 
-  done(null /*, data*/);
-};
+run().catch(console.dir);
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
